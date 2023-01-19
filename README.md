f80e1d564682a396e5ea687a058f555231663e7c
chris p <Christopherperez1820@gmail.com>
2023-01-19 08:55:40 -0500

    last commit for turn in

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+#  The Wireframes
+
+# Our Goal
+# Future goals
+
+
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav class="topbar" role="navigation">
+
+       <div className='navh1'> 
+       <a href='' div className='bar' > bars </a>
+       Portfolio
+       <a href='' div className='Home' > Home </a>
+        <a href='https://www.linkedin.com/in/christopher-perez-727888255/' div className='linkedin' > LI </a>
+        <a href='https://github.com/Cperez1820'  className='github'> github </a>
+       </div>
+       
+
+      </nav>
+
+
+    <header className="HomePage">Chris's Portfolio</header>
+    
+    <img src=''></img>
+    
     </div>
   );
 }
